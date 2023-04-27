e3eeb33f763ff35420a8dc84f16e2c324fda93a3
aservin6 <servin.alejandroii@gmail.com>
2023-04-27 10:45:28 -0500

    reduced transaction coin list to 20 instead of 250

--- src/components/Modals/TransactionModal/TransactionModal.jsx
+++ src/components/Modals/TransactionModal/TransactionModal.jsx
@@ -34,7 +34,7 @@ const TransactionModal = ({
       setError(null);
       try {
         const { data } = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false`
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`
         );
         setCoinListData(data);
         setIsLoading(false);
